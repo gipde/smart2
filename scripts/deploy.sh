1941475d018501b8e36ccfb475b540ee8c0277b4
Werner Schneider <werner@schneidernet.com>
2018-12-12 22:00:04 +0100

    little cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,7 @@
 echo "${SSH_KEY}" | base64 --decode >/tmp/ssh_rsa
 chmod 600 /tmp/ssh_rsa
 
-#CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o smart2
 CGO_ENABLED=0 go build -o smart2
-
-ls -lah
-ls scripts -lah
-ls /tmp -lah
 
 scp -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa smart2 scripts/Dockerfile werner@35.190.129.82:/home/werner/
 ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa werner@35.190.129.82 "docker build -t smart2 ."
