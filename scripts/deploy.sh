ad3c238f9b5270862dfcca338f8e1126be5cf2b5
Werner Schneider <werner@schneidernet.com>
2018-12-12 20:04:18 +0100

    Umstellung auf deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-ls -lah
-go build
-ls -lah
-pwd
-git status
-printenv
+
+echo "${SSH_KEY}" | base64 --decode >/tmp/ssh_rsa
+chmod 600 /tmp/ssh_rsa
+scp -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa ../smart2 Dockerfile werner@35.190.129.82:/home/werner/
+ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa werner@35.190.129.82 "docker build"
+
+shred /tmp/ssh_rsa
+
