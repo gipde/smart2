2e80a4ed2463ee9983809a5ebd6a00bef3dac110
Werner Schneider <werner@schneidernet.com>
2018-12-12 21:47:51 +0100

    start docker instance

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 echo "${SSH_KEY}" | base64 --decode >/tmp/ssh_rsa
 chmod 600 /tmp/ssh_rsa
 
@@ -12,6 +13,8 @@ ls /tmp -lah
 
 scp -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa smart2 scripts/Dockerfile werner@35.190.129.82:/home/werner/
 ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa werner@35.190.129.82 "docker build -t smart2 ."
+ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa werner@35.190.129.82 "docker stop smart2"
+ssh -o "StrictHostKeyChecking=no" -i /tmp/ssh_rsa werner@35.190.129.82 "docker run --name smart2 --rm  -p 80:1323 -d smart2"
 
 shred /tmp/ssh_rsa
 
